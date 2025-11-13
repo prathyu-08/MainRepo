eeb708f7a6a3ad783c0887fe129bba94eea0aaf8
prathyu-08 <vennapusaprathyusha08@gmail.com>
2025-11-13 18:10:39 +0530

    Delete 0182-duplicate-emails directory

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    duplicates = person.groupby('email').size()
-    duplicates = duplicates[duplicates > 1].reset_index()
-    duplicates.columns = ['Email', 'count']
-    result = duplicates[['Email']]
-    
-    return result
