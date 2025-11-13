f516d5cf90a2e90caa1b9a9648c1dcee154e69e6
prathyu-08 <vennapusaprathyusha08@gmail.com>
2025-11-13 18:10:52 +0530

    Delete 0183-customers-who-never-order directory

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def find_customers(Customers: pd.DataFrame, Orders: pd.DataFrame) -> pd.DataFrame:
-    result = Customers[~Customers['id'].isin(Orders['customerId'])]
-    result = result[['name']].rename(columns={'name': 'Customers'})
-    return result
