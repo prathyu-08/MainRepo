ff83d3802e30f5dd1ae1643d5f05e2a574edb4c3
prathyu-08 <vennapusaprathyusha08@gmail.com>
2025-11-13 19:36:16 +0530

    Delete pandas directory

--- pandas/2882-drop-duplicate-rows.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    result=customers.drop_duplicates(subset='email', keep='first')
-    return result
-    
--- pandas/3075-drop-missing-data/3075-drop-missing-data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    result=students.dropna(subset=['name'])
-    return result
-    
